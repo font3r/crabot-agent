385aaf12767295aab25e210d7c531f5886691ff9
fonter <serfin009@gmail.com>
2026-03-23 00:45:57 +0100

    feat: added sample api integration

---
+++ src/agents/currency_agent/currency_api.py
@@ -0,0 +1,31 @@
+import aiohttp
+from pydantic import BaseModel, ValidationError
+
+
+class ApiError(Exception):
+    def __init__(self, status: int, message: str):
+        self.status = status
+        self.message = message
+
+
+class ExchangeRateResponse(BaseModel):
+    amount: float
+    date: str
+    base: str
+    rates: dict[str, float]
+
+
+async def get_exchange_rate(
+    currency_from: str, currency_to: str
+) -> ExchangeRateResponse:
+    async with aiohttp.ClientSession("https://api.frankfurter.app/") as session:
+        resp = await session.get(
+            "latest", params={"from": currency_from, "to": currency_to}
+        )
+        if resp.status < 200 and resp.status > 299:
+            raise ApiError(status=resp.status, message=await resp.text())
+
+        try:
+            return ExchangeRateResponse.model_validate_json(await resp.text())
+        except ValidationError as e:
+            raise ApiError(status=resp.status, message=e.title)
--- src/command_handler.py
+++ src/command_handler.py
@@ -1,3 +1,4 @@
+from agents.currency_agent.currency_api import ApiError, get_exchange_rate
 from gateway_contracts import MessageEvent
 from rest_client import DiscordRestClient
 
@@ -16,6 +17,15 @@ async def handle_command(client: DiscordRestClient, msg: MessageEvent):
         await client.send_message(msg.channel_id, mention(269132306227265536))
     elif msg.content == "aha":
         await client.send_message(msg.channel_id, "ok")
+    elif msg.content == "currency":
+        try:
+            rates = await get_exchange_rate("EUR", "PLN")
+            await client.send_message(
+                msg.channel_id,
+                f"[{rates.date}] 1 {rates.base} -> {rates.rates['PLN']} PLN",
+            )
+        except ApiError as e:
+            await client.send_message(msg.channel_id, f"cannot fetch currency rate")
     else:
         await client.send_message(msg.channel_id, "invalid command")
 
